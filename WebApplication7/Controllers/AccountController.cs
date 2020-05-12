41fbc159af5d38ff6fe505d1f3d3c1db41a067c0
nisan candir <nisancandir366314@gmail.com>
2020-05-12 17:35:35 +0300

    initial commit

---
+++ WebApplication7/Controllers/AccountController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication7.Models;
+namespace WebApplication7.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Regis()
+        {
+
+            return View();
+        }
+    }
+}
